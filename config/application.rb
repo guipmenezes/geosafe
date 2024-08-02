e6af8a961828dfee79f665b60f7c77d1ff8121ca
guipmenezes <guimenezesunb@gmail.com>
2024-08-01 23:52:29 -0300

    :wrench: updates database file and locale to a brazilian one

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,11 @@ module Geosafe
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Brasilia"
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :"pt-BR"
+    I18n.enforce_available_locales = false
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
