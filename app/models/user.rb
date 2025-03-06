89c51318f6942fe081cf17f50cd156c4742e0572
guipmenezes <guimenezesunb@gmail.com>
2025-03-05 23:16:13 -0300

    creates the user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,19 @@ class User < ApplicationRecord
 
 
   has_many :sessions, dependent: :destroy
+  has_one :address, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, allow_nil: true, length: { minimum: 12 }
+  validates :password_confirmation, presence: true, if: :password_digest_changed?
+  validate :password_matches_confirmation
+  validates :full_name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 },
+                      format: { with: /\A[a-zA-Z0-9_]+\z/, message: "apenas permite letras, números e underline" }
 
   normalizes :email, with: -> { _1.strip.downcase }
+  normalizes :username, with: -> { _1.strip.downcase }
+  normalizes :full_name, with: -> { _1.strip }
 
   before_validation if: :email_changed?, on: :update do
     self.verified = false
@@ -22,5 +30,13 @@ class User < ApplicationRecord
 
   after_update if: :password_digest_previously_changed? do
     sessions.where.not(id: Current.session).delete_all
+  end
+
+  private
+
+  def password_matches_confirmation
+    return if password.blank? || password_confirmation.blank?
+    return if password == password_confirmation
+    errors.add(:password_confirmation, "deve ser igual à senha")
   end
 end
