d29089c9dbca77bb30d864ce07c42e1b378b00e5
guipmenezes <guimenezesunb@gmail.com>
2025-07-22 12:33:25 -0300

    feat: implement hamburger menu for improved tablet responsiveness

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "menu", "button" ]
+
+  toggle() {
+    this.menuTarget.classList.toggle("hidden")
+  }
+}
