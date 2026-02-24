dc9bc6eb21751b1fad83b18bedc8d6dfd2f6df7a
guipmenezes <guimenezesunb@gmail.com>
2026-02-23 23:30:28 -0300

    fixes the action of editing an alert

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -25,6 +25,11 @@ class AlertsController < ApplicationController
 
   def edit
     authorize @alert
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   def update
