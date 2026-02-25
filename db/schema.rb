44b3669824168a6a626a9700360bb91dc7d50cdf
guipmenezes <guimenezesunb@gmail.com>
2026-02-24 22:50:13 -0300

    commiting schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_16_025600) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_17_011919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_16_025600) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
   end
 
   create_table "plan_subscriptions", force: :cascade do |t|
