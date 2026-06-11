031dc4ac2dc9366c67d62f361f7f7d5a0e9d3f3a
guipmenezes <guimenezesunb@gmail.com>
2026-06-10 23:07:34 -0300

    fixes helper for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -53,7 +53,7 @@ RSpec.configure do |config|
 
   config.include ViewComponent::TestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
-  config.include ActionView::Helpers::AssetUrlHelper
+  config.include ActionView::Helpers::AssetUrlHelper, type: :component
   config.include ControllerMacros, type: :request
   config.include SystemHelpers, type: :system
   config.include ActiveSupport::Testing::TimeHelpers
