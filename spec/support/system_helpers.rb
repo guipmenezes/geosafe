0771dde944476ab172767c7c48b52969455076ef
guipmenezes <guimenezesunb@gmail.com>
2026-06-10 23:25:26 -0300

    fixes rubocop

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -2,12 +2,7 @@
 
 module SystemHelpers
   def sign_in_as(user)
-    # Force resize to desktop to ensure lg: classes are active
-    if page.driver.respond_to?(:resize)
-      page.driver.resize(1200, 800)
-    elsif page.driver.browser.respond_to?(:resize)
-      page.driver.browser.resize(window_size: [1200, 800])
-    end
+    ensure_desktop_viewport
 
     visit '/sign_in'
     fill_in 'email', with: user.email
@@ -15,7 +10,16 @@ module SystemHelpers
     click_on 'ENTRAR'
 
     # Wait for login to complete and redirect to the home page
-    # Using visible: true to ensure we are on desktop view
     expect(page).to have_text("Olá, #{user.full_name.split.first}", wait: 10)
+  end
+
+  private
+
+  def ensure_desktop_viewport
+    if page.driver.respond_to?(:resize)
+      page.driver.resize(1200, 800)
+    elsif page.driver.browser.respond_to?(:resize)
+      page.driver.browser.resize(window_size: [1200, 800])
+    end
   end
 end
