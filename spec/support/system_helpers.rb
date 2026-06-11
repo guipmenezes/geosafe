f2ab0816db38c847a12ccfe60854bc2db265f161
guipmenezes <guimenezesunb@gmail.com>
2026-06-10 23:16:49 -0300

    fixes rubocop

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -6,7 +6,7 @@ module SystemHelpers
     fill_in 'email', with: user.email
     fill_in 'password', with: 'Secret1*3*5*'
     click_on 'ENTRAR'
-    
+
     # Wait for login to complete and redirect to the home page
     expect(page).to have_text("Olá, #{user.full_name.split.first}")
   end
