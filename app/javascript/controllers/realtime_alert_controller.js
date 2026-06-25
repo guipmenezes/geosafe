5a80949e17b1698f09dd43f48edcb8d5c41d4797
guipmenezes <guimenezesunb@gmail.com>
2026-06-24 23:16:39 -0300

    fix map infinite consoles

--- app/javascript/controllers/realtime_alert_controller.js
+++ app/javascript/controllers/realtime_alert_controller.js
@@ -4,7 +4,9 @@ export default class extends Controller {
   static values = { data: Object }
 
   connect() {
-    if (this.hasDataValue) {
+    // Only dispatch once per element to prevent infinite loops when elements are moved/sorted in the DOM
+    if (this.hasDataValue && !this.element.dataset.alertDispatched) {
+      this.element.dataset.alertDispatched = "true"
       window.dispatchEvent(new CustomEvent("alert:created", {
         detail: { alert: this.dataValue }
       }))
