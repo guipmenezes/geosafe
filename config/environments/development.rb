8e3f164c7562bc74857e350544bf8bd0ec2ccf1c
guipmenezes <guimenezesunb@gmail.com>
2026-06-29 23:23:16 -0300

    configs host to access from smartphone

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Allow access from local network (mobile, etc)
+  config.hosts.clear
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
